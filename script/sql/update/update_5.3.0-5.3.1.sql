0c6faa751ac16a3e1c3dfa7c2648aceeba170385
疯狂的狮子Li <15040126243@163.com>
2025-03-04 09:34:17 +0800

    update 删除无用sql

--- script/sql/update/update_5.3.0-5.3.1.sql
+++ script/sql/update/update_5.3.0-5.3.1.sql
@@ -1,6 +1,3 @@
 ALTER TABLE `flow_node` DROP COLUMN `skip_any_node`;
 ALTER TABLE `flow_node`
     ADD COLUMN `ext` text NULL COMMENT '扩展属性' AFTER `update_time`;
-
-INSERT INTO sys_dict_type VALUES (16, '000000', '按钮权限', 'wf_button_permission', 103, 1, sysdate(), NULL, NULL, '按钮权限列表');
-INSERT INTO sys_dict_data VALUES (60, '000000', 1, '是否弹窗选人', '1', 'wf_button_permission', '', 'default', 'N', 103, 1, sysdate(), NULL, NULL,'是否弹窗选人');
