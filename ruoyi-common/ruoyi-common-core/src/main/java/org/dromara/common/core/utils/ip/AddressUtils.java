f29e0223a7485d797c3715ffe8b17ea8c1c7e39f
秋辞未寒 <545073804@qq.com>
2025-04-26 00:11:58 +0800

    update 优化 获取地址支持IPv6判断而不是抛异常

--- ruoyi-common/ruoyi-common-core/src/main/java/org/dromara/common/core/utils/ip/AddressUtils.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/org/dromara/common/core/utils/ip/AddressUtils.java
@@ -1,11 +1,11 @@
 package org.dromara.common.core.utils.ip;
 
-import cn.hutool.core.net.NetUtil;
 import cn.hutool.http.HtmlUtil;
-import org.dromara.common.core.utils.StringUtils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dromara.common.core.utils.NetUtils;
+import org.dromara.common.core.utils.StringUtils;
 
 /**
  * 获取地址类
@@ -20,14 +20,17 @@ public class AddressUtils {
     public static final String UNKNOWN = "XX XX";
 
     public static String getRealAddressByIP(String ip) {
-        if (StringUtils.isBlank(ip)) {
+        // 处理空串并过滤HTML标签
+        ip = HtmlUtil.cleanHtmlTag(StringUtils.blankToDefault(ip,""));
+        // 判断是否为IPv4或IPv6，如果不是则返回未知地址
+        if (!NetUtils.isIPv4(ip) && !NetUtils.isIPv6(ip)) {
             return UNKNOWN;
         }
         // 内网不查询
-        ip = StringUtils.contains(ip, "0:0:0:0:0:0:0:1") ? "127.0.0.1" : HtmlUtil.cleanHtmlTag(ip);
-        if (NetUtil.isInnerIP(ip)) {
+        if (NetUtils.isInnerIPv6(ip) || NetUtils.isInnerIP(ip)) {
             return "内网IP";
         }
         return RegionUtils.getCityInfo(ip);
     }
+
 }
