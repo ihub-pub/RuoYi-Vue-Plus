777ae645c5f3a0792c46b436db870ca1c1a86793
songgaoshuai <1742057357@qq.com>
2025-05-19 11:31:01 +0800

    update 手动合并冲突 优化 工作流模块下一个节点指定办理人、角色和部门转具体用户、抄送人和消息推送，改到通过全局分派监听器和完成监听器处理 update 修复退回申请人无法发送消息问题

--- ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/service/IFlwCommonService.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/service/IFlwCommonService.java
@@ -1,10 +1,6 @@
 package org.dromara.workflow.service;
 
-import org.dromara.warm.flow.core.entity.User;
-import org.dromara.warm.flow.core.service.UserService;
-
 import java.util.List;
-import java.util.Set;
 
 /**
  * 通用 工作流服务
@@ -14,29 +10,12 @@ import java.util.Set;
 public interface IFlwCommonService {
 
     /**
-     * 获取工作流用户service
-     *
-     * @return 工作流用户service
-     */
-    UserService getFlowUserService();
-
-    /**
      * 构建工作流用户
      *
-     * @param userList 办理用户
-     * @param taskId   任务ID
+     * @param permissionList 办理用户
      * @return 用户
      */
-    Set<User> buildUser(List<User> userList, Long taskId);
-
-    /**
-     * 构建工作流用户
-     *
-     * @param userIdList 办理用户
-     * @param taskId     任务ID
-     * @return 用户
-     */
-    Set<User> buildFlowUser(List<String> userIdList, Long taskId);
+    List<String> buildUser(List<String> permissionList);
 
     /**
      * 发送消息
